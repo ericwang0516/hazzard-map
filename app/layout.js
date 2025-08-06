35ff920a8ef7d7781afb486db709432271d7b10a
ericwang0516 <ericwang1156@gmail.com>
2025-08-06 16:25:09 +0900

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -29,7 +29,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" data-by="Eric :D">
       <head>
       <SpeedInsights/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
